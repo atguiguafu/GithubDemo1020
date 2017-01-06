6b15455d5b13058c31fd5750696300ed5ef362ee
atguiguafu <541433511@qq.com>
2017-01-06 11:05:27 +0800

    是否涛声依旧

--- app/src/main/java/com/atguigu/githubdemo1020/MainActivity.java
+++ app/src/main/java/com/atguigu/githubdemo1020/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "是否涛声依旧", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "是否涛声依旧", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "是否涛声依旧", Toast.LENGTH_SHORT).show();
     }
 }
